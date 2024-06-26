577d5658cbc031df258be67530259054262fea3a
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 15:38:38 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -158,7 +158,7 @@ const Login = () => {
    <div className='mb-3'>
        <div className="input-group" >
             <div className="google-auth">
-  <a href="http://localhost:3500/auth/google" style={{color:'white',textDecoration: 'none'}}>
+  <a href="https://blogsite-3gsy.onrender.com/auth/google" style={{color:'white',textDecoration: 'none'}}>
         <FcGoogle style={{fontSize:'35px'}}/>
         <span style={{paddingLeft:'10px'}}>Sign In with Google</span>
       </a>
