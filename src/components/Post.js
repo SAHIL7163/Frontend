88ad250743bf2e53cf6f02356816b2640f40c540
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-09-10 18:22:43 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import useWindowsize from '../hooks/useWindowsize';
 import { useNavigate } from 'react-router-dom';
 
+
 const Post = ({post,postIndex}) => {
   const navigate=useNavigate();
   const {width}=useWindowsize();
   const categories =['Travel','Tech','Finance','Fashion','Food','PesonalBlog','Sports'];
-      const imageUrl = post.imageUrl.replace(/\\/g, '/');
   
   const handleReadMoreClick = async () => {
     // Navigate to the root URL
@@ -17,14 +17,22 @@ const Post = ({post,postIndex}) => {
 
   };
 
+  function removeHtmlTags(str) {
+    return str.replace(/<[^>]*>/g, '');
+}
+   const cleanString = removeHtmlTags(post.body);
+
+
+
+
+  const truncatedText = cleanString.length <=25 ? cleanString : `${(cleanString).slice(0,200)}...` ;  // Adjust the slice length as needed
   return (
    <article className="post">
     <div className="post-container">
     <div className='post-image-1 d-flex flex-column justify-content-center'>
       <div>
           {(postIndex % 2 !== 0  ||  width <=768 )? (
-        
-            <img className="img-fluid" src={`https://blogsite-api-mi38.onrender.com/${imageUrl}`} alt='' />
+            <img className="img-fluid" src={`http://localhost:3500/${post.imageUrl}`} alt='' />
           ) : null}
         </div>
     </div>
@@ -33,13 +41,7 @@ const Post = ({post,postIndex}) => {
 {/*      <h5>{post._id}</h5> */}
     <p className="postDate" >{post.datetime}</p>
 <p className="postcategory">{categories[post.categoryId]}</p>
-   
-     <p className="postBody fs-5">{
-       (post.body).length <= 25 
-        ? post.body
-        :`${(post.body).slice(0,140)}...`
-     }
-     </p>
+<p>{truncatedText}</p> 
      <div className='post-button'>
       <button  className='btn btn-lg btn-primary' onClick={handleReadMoreClick}>Read More</button>
        </div>
@@ -49,7 +51,7 @@ const Post = ({post,postIndex}) => {
    </div>  */}
      <div className='post-image-2 d-flex flex-column justify-content-center'>
           {(postIndex % 2 === 0  && width>768)? (
-            <img className="img-fluid" src={`https://blogsite-api-mi38.onrender.com/${imageUrl}`} alt='' />
+            <img className="img-fluid" src={`http://localhost:3500/${post.imageUrl}`} alt='' />
           ) : null}
         </div>
     </div>
